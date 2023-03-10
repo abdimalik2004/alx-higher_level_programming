0999aa13e7c4c3db1469013fbe9e94aa0747ed26
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-03-10 10:40:59 -0800

    0-add.py file added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3"
-def add(a, b):
-    Args:
-        a: first integer
-        b: second integer
+#!/usr/bin/python3
 
-        Returns:
-            The value. a+b
-            return (a + b)
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
